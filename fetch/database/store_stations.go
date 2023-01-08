14e48aea039a7523ca24fd9055c58295880a6dd1
Douglas Braga <30268535+dobraga@users.noreply.github.com>
2023-01-08 17:00:29 -0300

    feat: reduce batch size

--- fetch/database/store_stations.go
+++ fetch/database/store_stations.go
@@ -8,5 +8,5 @@ import (
 
 func StoreStations(stations []models.Station) error {
 	db := Connect()
-	return db.Clauses(clause.OnConflict{UpdateAll: true}).CreateInBatches(stations, 500).Error
+	return db.Clauses(clause.OnConflict{UpdateAll: true}).CreateInBatches(stations, 50).Error
 }
